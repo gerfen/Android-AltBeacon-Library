ecd0742f07d1cff9b943ed13795b14a47cc88e6c
Michael Gerfen <gerfen@hotmail.com>
2024-11-18 17:04:34 -0800

    Updated to support Android 15 and android-beacon-library-2.20.7

---
+++ AndroidAltBeaconLibrary/AndroidAltBeaconLibrary/ChangeAwareCopyOnWriteArrayList.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AltBeaconOrg.BoundBeacon.Utils
+{
+    public partial class ChangeAwareCopyOnWriteArrayList
+    {
+
+        //public new int Size()
+        //{
+        //    _Size();
+        //}
+    }
+}
